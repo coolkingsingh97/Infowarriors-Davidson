8376412ff81e6da00786665e762f2e040b32fe66
Jayashree Raman <ramankjayashree@gmail.com>
2019-02-05 10:36:59 -0800

    Update scrape_filings.py

--- scrape_filings.py
+++ scrape_filings.py
@@ -1,3 +1,4 @@
+# Install edgar package if not not already installed using "pip install edgar"
 import edgar
 
 edgar.download_index('C:\\Users\\Jayashree RAMAN\\Documents\\DellDocs\\Capstone\\EDGAR_Downloads', 2018)
